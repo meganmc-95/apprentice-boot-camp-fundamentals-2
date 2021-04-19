6d9eda0efa21a9f659d232586de87da50a07abb1
Megan McClellan <Megan_McClellan@waters.com>
2021-04-19 15:32:33 +0100

    Add beforeeach in refactor. Add strike test &logic

--- exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingTests.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingTests.cs
@@ -4,9 +4,14 @@ namespace McrDigital.Bootcamp1.Checkout.Tests
 {
     public class BowlingGameTests
     {
+
+        private Bowling bowling;
+        public BowlingGameTests() {
+            this.bowling = new Bowling();
+        }
+
         [Fact]
         public void ShouldCalculateATotalScoreOfZeroWithNoPinsKnockedDown(){
-            var bowling = new Bowling();
             var rolls = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
             var totalScore = bowling.Score(rolls);
@@ -16,21 +21,29 @@ namespace McrDigital.Bootcamp1.Checkout.Tests
 
         [Fact]
         public void ShouldCalculateATotalScoreWithVariedPinsKnockedDown(){
-            var bowling = new Bowling();
             var rolls = new int[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
             var totalScore = bowling.Score(rolls);
 
             Assert.Equal(20, totalScore);
         }
+
         [Fact]
         public void ShouldCalculateATotalScoreWithSpareIncluded(){
-            var bowling = new Bowling();
             var rolls = new int[] {5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
             var totalScore = bowling.Score(rolls);
 
             Assert.Equal(12, totalScore);
+        }
+
+        [Fact]
+        public void ShouldCalculateATotalScoreWithStrikeIncluded(){
+            var rolls = new int[] {10, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            var totalScore = bowling.Score(rolls);
+
+            Assert.Equal(14, totalScore);
         }
     }
 }
--- exercises/dotnet/McrDigital.Bootcamp1.Checkout/Bowling.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Bowling.cs
@@ -1,20 +1,33 @@
 namespace McrDigital.Bootcamp1.Checkout {
   public class Bowling {
+
+      public bool isSpare(int first, int second) {
+          return ((first + second) == 10) && (first != 10);
+      }
+
+      public bool isStrike(int first) {
+          return first == 10;
+      }
+
       public int Score(int[] rolls) {
           var score = 0;
+          int increment = 2;
 
-          for(int i = 0; i < rolls.Length; i += 2) {
+          for(int i = 0; i < rolls.Length; i += increment) {
+              increment = 2;
               var firstRoll = rolls[i];
               var secondRoll = rolls[i + 1];
-              var spareStrike = firstRoll + secondRoll;
 
               score += firstRoll;
               score += secondRoll;
 
-              if(spareStrike == 10) {
+              if(isStrike(firstRoll)) {
+                increment = 1;
+                score += rolls[i + 2];
+                score += rolls[i + 3];
+              } else if(isSpare(firstRoll, secondRoll)) {
                   score += rolls[i + 2];
-              }
-
+              } 
           }
 
           return score;
